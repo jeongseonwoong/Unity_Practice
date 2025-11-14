720e7f98c0a9781060e3a85f14d80d4850f606be
jeongseonwoong <tjsdnd1219@naver.com>
2025-11-14 16:14:38 +0900

    feat(Operation): 연산을 사용자의 x, y, z 입력을 받도록 변경

--- game-server/global/event/src/main/java/backend/cowrite/common/event/payload/Operation.java
+++ game-server/global/event/src/main/java/backend/cowrite/common/event/payload/Operation.java
@@ -1,12 +1,9 @@
 package backend.cowrite.common.event.payload;
 
-import com.fasterxml.jackson.annotation.JsonSubTypes;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-
-@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
-@JsonSubTypes({
-        @JsonSubTypes.Type(value = DeleteOperation.class, name = "DELETE"),
-        @JsonSubTypes.Type(value = InsertOperation.class, name = "INSERT")
-})
-public interface Operation {
+public class Operation {
+    OperationType type = OperationType.MOVE;
+    Long playerId;
+    Double x;
+    Double y;
+    Double z;
 }
